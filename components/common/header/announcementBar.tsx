dae1e82b8269fa018b3b3e123f3a7a072bf52886
thinm98 <nguyenthi10898@gmail.com>
2025-09-19 23:06:06 +0700

    update

--- components/common/header/announcementBar.tsx
+++ components/common/header/announcementBar.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import "./styles.scss";
+
 const AnnouncementBar = () => {
   return (
-    <div className="flex gap-8">
-      <div className="shrink-0">test 1</div>
+    <div className="flex gap-8 text-xs">
+      <div className="hidden lg:block shrink-0 basic-1/4">test 1</div>
       <div className="flex-1 text-center">
         FREE EXPRESS SHIPPING ABOVE US$300 & FREE 30 DAY RETURNS - NEVER ON SALE
       </div>
-      <div className="shrink-0">test 3</div>
+      <div className="hidden lg:block shrink-0 basic-1/4">test 3</div>
     </div>
   );
 };
---
+++ src/app/product-detail/page.tsx
@@ -0,0 +1,7 @@
+import ProductDetail from "../../../components/productDetail";
+
+const ProductDetailPage = () => {
+  return <ProductDetail />;
+};
+
+export default ProductDetailPage;
